844f66b378464e5370015d21f65bb61f6b92e709
Mor-gan <ogundipeabimbola9@gmail.com>
2026-07-30 15:50:27 +0100

    provider-aws

---
+++ Terraform/retainer-project/main.tf
@@ -0,0 +1,12 @@
+# main.tf
+# Sensitive Data Retention Project
+
+locals {
+  project_name = "sensitive-data-retention"
+
+  common_tags = {
+    Project     = local.project_name
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
