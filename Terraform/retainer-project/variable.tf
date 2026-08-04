9785ae14c9ff449117b56eb9f80eab354d78c928
Mor-gan <ogundipeabimbola9@gmail.com>
2026-08-04 15:51:21 +0100

    decalred varibles

---
+++ Terraform/retainer-project/variable.tf
@@ -0,0 +1,43 @@
+# variables.tf
+
+variable "aws_region" {
+  description = "AWS region where resources will be deployed"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "sensitive-data-retention"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "source_bucket_name" {
+  description = "S3 bucket where users upload their files"
+  type        = string
+  default     = "sensitive-data-source-bucket"
+}
+
+variable "quarantine_bucket_name" {
+  description = "S3 bucket used to store sensitive files before permanent deletion."
+  type        = string
+  default     = "sensitive-data-quarantine-bucket"
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "sensitive-data-scanner"
+}
+
+variable "retention_days" {
+  description = "Number of days sensitive files should be retained"
+  type        = number
+  default     = 30
+}
