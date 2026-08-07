b1b6581e23923f3e193282cb6b9a0b4512e19fab
Mor-gan <ogundipeabimbola9@gmail.com>
2026-08-07 15:49:41 +0100

    generated a lamda function IAC

--- Terraform/retainer-project/lambda.tf
+++ Terraform/retainer-project/lambda.tf
@@ -16,3 +16,36 @@ resource "aws_cloudwatch_log_group" "lambda_logs" {
     Environment = var.environment
   }
 }
+
+# Lambda Function
+
+resource "aws_lambda_function" "scanner" {
+
+  function_name = var.lambda_function_name
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  role    = aws_iam_role.lambda_role.arn
+  handler = "lambda_function.lambda_handler"
+
+  runtime = "python3.12"
+  timeout = 60
+  memory_size = 256
+
+  environment {
+    variables = {
+      SOURCE_BUCKET      = aws_s3_bucket.source_bucket.bucket
+      QUARANTINE_BUCKET  = aws_s3_bucket.quarantine_bucket.bucket
+      RETENTION_DAYS     = var.retention_days
+    }
+  }
+
+  depends_on = [
+    aws_cloudwatch_log_group.lambda_logs
+  ]
+
+  tags = {
+    Environment = var.environment
+  }
+}
